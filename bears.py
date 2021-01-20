42a75b804c34c5f473f49d9c1c2aef96d21bc841
Nilay Patel <nilaypatel@Nilays-MacBook-Pro.local>
2021-01-19 18:59:53 -0700

    Done

--- bears.py
+++ bears.py
@@ -1,1 +1,22 @@
 def bears(n):
+
+    if (n == 42):
+        return True
+
+    if (n < 42):
+        return False
+
+    if (n % 5 is 0) and bears(n - 42):
+        return True
+
+    elif (n % 2 is 0) and bears(n / 2):
+        return True
+
+    elif (n % 4 is 0) or (n % 3 is 0):
+        
+        a = n % 10
+        b = (n % 100) / 10
+        return a * b != 0 and bears(n - a * b)
+
+    return False
+
