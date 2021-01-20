241e1a5c8096fbc1ff3306a7f3e953c25d550602
Nilay Patel <nilaypatel@Nilays-MacBook-Pro.local>
2021-01-19 19:18:27 -0700

    Pass

--- base_convert_testcases.py
+++ base_convert_testcases.py
@@ -5,12 +5,17 @@ class TestBaseConvert(unittest.TestCase):
 
     def test_base2(self):
         self.assertEqual(convert(45,2),"101101")
+        pass
 
     def test_base4(self):
         self.assertEqual(convert(30,4),"132")
+        pass
 
     def test_base16(self):
         self.assertEqual(convert(316,16),"13C")
+        pass
 
 if __name__ == "__main__":
         unittest.main()
+
+          
