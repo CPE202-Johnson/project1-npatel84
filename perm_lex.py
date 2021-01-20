f56f004a1e78c56cca90f1828681fd467dd1bf7e
Nilay Patel <nilaypatel@Nilays-MacBook-Pro.local>
2021-01-19 19:06:42 -0700

    Done

--- perm_lex.py
+++ perm_lex.py
@@ -1,3 +1,22 @@
 def perm_gen_lex(a): 
 
+    if a is 0:
+        return []
+    
+    if len(a) == 1:
+        return [a]
+    
+    a_list = perm_gen_lex(a[1:])
+    char = a[0]
+    result = []
+
+    for perm in a_list:
+        for i in range(len(perm) +1):
+            result.append(perm[:i] + char + perm[i:])
+
+    return sorted(result)
+
+
+
+
  
