b25552716087a38e4226506c0f811f5af4073026
Nilay Patel <nilaypatel@Nilays-MacBook-Pro.local>
2021-01-21 16:44:00 -0700

    Late submit

--- base_convert_testcases.py
+++ base_convert_testcases.py
@@ -11,6 +11,26 @@ class TestBaseConvert(unittest.TestCase):
         
     def test_base16(self):
         self.assertEqual(convert(316,16),"13C")
+    
+    def test_base11(self):
+        self.assertEqual(convert(21,11),"1A")
+    
+    def test_base12(self):
+        self.assertEqual(convert(23,12),"1B")
+    
+    def test_base13(self):
+        self.assertEqual(convert(25,13),"1C")
+    
+    def test_base14(self):
+        self.assertEqual(convert(27,14),"1D")
+    
+    def test_base15(self):
+        self.assertEqual(convert(29,15),"1E")
+    
+    def test_base16(self):
+        self.assertEqual(convert(31,16),"1F")
+    
+
         
 
 if __name__ == "__main__":
