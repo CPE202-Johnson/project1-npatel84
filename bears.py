b8f5dbcc15fe6eecc2eb8c2ebab35106591bae71
Nilay Patel <nilaypatel@Nilays-MacBook-Pro.local>
2021-01-21 16:30:13 -0700

    Late364

--- bears.py
+++ bears.py
@@ -15,7 +15,7 @@ def bears(n):
     elif (n % 4 is 0) or (n % 3 is 0):
         
         a = n % 10
-        b = (n % 100) / 10
+        b = (n % 100) // 10
         return (a * b != 0) and bears(n - a * b)
 
     return False
